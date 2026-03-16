9d566a5e2de8942f93cb1f74f77f4530407a14e1
Kirtikumar78 <kirtikumarp25@gmail.com>
2026-03-16 22:15:17 +0530

    new commit

---
+++ src/Strings/CapitalizeFirstLast.java
@@ -0,0 +1,32 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class CapitalizeFirstLast {
+    public static String CapitalizeFirstLastWord(String str){
+        String []words=str.split(" ");
+        String result="";
+        for(int i=0;i<words.length;i++){
+            String word=words[i];
+            if(word.length()==0){
+                result=result+Character.toUpperCase(word.charAt(0))+" ";
+
+            }
+            else{
+                char first=Character.toUpperCase(word.charAt(0));
+                char last=Character.toUpperCase(word.charAt(word.length()-1));
+                String middle=word.substring(1,word.length()-1);
+                result=result+first+middle+last+" ";
+
+            }
+        }
+        return result.trim();
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String sentence=sc.nextLine();
+        String str=CapitalizeFirstLastWord(sentence);
+        System.out.println(str);
+
+    }
+}
