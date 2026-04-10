d04e3a02a070ae622117fcb2a7b419a7d1e98b1a
Kirtikumar78 <kirtikumarp25@gmail.com>
2026-04-10 23:20:58 +0530

    new commit

---
+++ src/Numbers/GreatestNumber.java
@@ -0,0 +1,13 @@
+package Numbers;
+
+public class GreatestNumber {
+    public static void main(String[]args){
+        int n=4;
+        int k=6;
+        if(n>k){
+            System.out.println(n);
+        }else{
+            System.out.println(k);
+        }
+    }
+}
