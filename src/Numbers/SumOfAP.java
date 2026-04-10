b970450d5256dab8a57a4e051ad4ee41786a36b2
Kirtikumar78 <kirtikumarp25@gmail.com>
2026-04-10 23:19:35 +0530

    new commit

---
+++ src/Numbers/SumOfAP.java
@@ -0,0 +1,11 @@
+package Numbers;
+
+public class SumOfAP {
+    public static void main(String[]args){
+        int n=4;
+        int a=2;
+        int d=2;
+        int sum=(n*(2*a+(n-1)*d))/2;
+        System.out.println(sum);
+    }
+}
